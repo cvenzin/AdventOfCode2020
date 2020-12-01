ecd7ab4cea33cc7350467537bc8c89c3be3f7509
Christian Venzin <christian.venzin@hexagon.com>
2020-12-01 12:34:47 +0100

    avoid self comparison

--- day01.js
+++ day01.js
@@ -202,10 +202,10 @@ const input = `1150
 const numbers = input.split('\n').map(n => Number(n));
 
 function part1() {
-    for (const n1 of numbers) {
-        for (const n2 of numbers) {
-            if ((n1 + n2) === 2020) {
-                return n1 * n2;
+    for (let i1 = 0; i1 < numbers.length; i1++) {
+        for (let i2 = i1; i2 < numbers.length; i2++) {
+            if ((numbers[i1] + numbers[i2]) === 2020) {
+                return numbers[i1] * numbers[i2];
             }
         };
     };
@@ -213,14 +213,17 @@ function part1() {
 console.log(part1());
 
 function part2() {
-    for (const n1 of numbers) {
-        for (const n2 of numbers) {
-            for (const n3 of numbers) {
-                if ((n1 + n2 + n3) === 2020) {
-                    return n1 * n2 * n3;
+    for (let i1 = 0; i1 < numbers.length; i1++) {
+        for (let i2 = i1; i2 < numbers.length; i2++) {
+            for (let i3 = i2; i3 < numbers.length; i3++) {
+                if ((numbers[i1] + numbers[i2] + numbers[i3]) === 2020) {
+                    return numbers[i1] * numbers[i2] * numbers[i3];
                 }
             };
         };
     };
 };
 console.log(part2());
+
+// 1014624
+// 80072256

29b4701f8adb2314402af6b87b0c8d97c4731195
Christian Venzin <christian.venzin@hexagon.com>
2020-12-02 12:22:12 +0100

    day02 fixes

--- source/day02/code.js
+++ source/day02/code.js
@@ -5,20 +5,20 @@ const input = fs.readFileSync('source/day02/input.txt', 'utf-8');
 const lines = input.split('\n');
 const data = [];
 lines.forEach(line => {
-    const line = {
+    const lineData = {
         min: 0,
         max: 0,
         char: 'a',
         pwChars: []
     };
     const parts = line.split(':');
-    line.pwChars = parts[1].trim().split('');
+    lineData.pwChars = parts[1].trim().split('');
     const parts2 = parts[0].split(' ');
-    line.char = parts2[1];
+    lineData.char = parts2[1];
     const numbers = parts2[0].split('-').map(n => Number(n));
-    line.min = numbers[0];
-    line.max = numbers[1];
-    data.push(line);
+    lineData.min = numbers[0];
+    lineData.max = numbers[1];
+    data.push(lineData);
 });
 
 function part1() {
@@ -38,7 +38,7 @@ function part2() {
     data.forEach(d => {
         const minIndex = d.min - 1;
         const maxIndex = d.max - 1;
-        if (d.pwChars[minIndex] === d.char && d.pwChars[maxIndex] !== d.char || d.pwChars[minIndex] !== d.char && d.pwChars[maxIndex] === d.char) {
+        if (d.pwChars[minIndex] === d.char ^ d.pwChars[maxIndex] === d.char) {
             validPwCount++;
         }
     });

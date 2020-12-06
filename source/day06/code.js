219b7f45524acfcf8dc7216cce8868bfdd330545
Christian Venzin <christian.venzin@hexagon.com>
2020-12-06 13:44:59 +0100

    refactoring

--- source/day06/code.js
+++ source/day06/code.js
@@ -1,53 +1,21 @@
 import {
     getLines
-} from '../modules/input.js'
+} from '../modules/input.js';
 
 const lines = getLines('day06');
 
 function part1() {
-    const groupAnswers = [new Set()];
-    let currentGroupAnswer = groupAnswers[0];
-    lines.forEach(line => {
-        if (line) {
-            line.split('').forEach(char => {
-                currentGroupAnswer.add(char);
-            });
-        } else {
-            currentGroupAnswer = new Set();
-            groupAnswers.push(currentGroupAnswer);
-        }
-    });
-    return groupAnswers.map(groupAnswer => groupAnswer.size).reduce((accumulator, currentValue) => accumulator + currentValue);
+    const mergedGroups = lines.map(l => l === '' ? '\n' : l).join('').split('\n');
+    return mergedGroups.map(group => [...new Set(group)].length).reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 console.log(part1());
 
+function getCommonValuesOfGroupAnswers(answers) {
+    return answers.map(a => a.split('')).reduce((p, c) => p.filter(e => c.includes(e)));
+}
+
 function part2() {
-    const groupAnswers = [new Set()];
-    let currentGroupAnswer = groupAnswers[0];
-    let groupCompleted = false;
-    lines.forEach(line => {
-        if (line && !groupCompleted) {
-            const newValues = line.split('');
-            if (currentGroupAnswer.size === 0) {
-                newValues.forEach(newValue => {
-                    currentGroupAnswer.add(newValue);
-                });
-            } else {
-                for (let item of currentGroupAnswer) {
-                    if (!newValues.find(newValue => newValue === item)) {
-                        currentGroupAnswer.delete(item);
-                        if (currentGroupAnswer.size == 0) {
-                            groupCompleted = true; // no need to process the other lines for this group
-                        }
-                    }
-                }
-            }
-        } else if (!line) {
-            groupCompleted = false;
-            currentGroupAnswer = new Set();
-            groupAnswers.push(currentGroupAnswer);
-        }
-    });
-    return groupAnswers.map(groupAnswer => groupAnswer.size).reduce((accumulator, currentValue) => accumulator + currentValue);
+    const groups = lines.map(l => l === '' ? '\n' : l).join(' ').split(' \n ').map(l => l.split(' '));
+    return groups.map(answers => getCommonValuesOfGroupAnswers(answers).length).reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 console.log(part2());

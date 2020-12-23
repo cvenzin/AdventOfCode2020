e2ebcc8b7f57a7f6830185c11cceae30e7f419b2
Christian Venzin <christian.venzin@hexagon.com>
2020-12-23 22:47:35 +0100

    perf fix

--- source/day23/code.js
+++ source/day23/code.js
@@ -5,41 +5,30 @@ import {
 const lines = getLines('day23');
 const cups = lines[0].split('').map(Number);
 
-function compute2(cupList, iterations, part2) {
+function compute(cupList, iterations) {
 
-    // build map and linked list
-    const map = {};
-    let currentNode = {
-        data: cupList[0]
-    };
-    map[cupList[0]] = currentNode;
-    for (let i = 1; i < cupList.length; i++) {
-        currentNode.next = {
-            data: cupList[i]
-        };
-        map[cupList[i]] = currentNode.next;
-        currentNode = currentNode.next;
+    // build data structure
+    const map = []; // index is cup label, value is index of next cup
+    for (let i = 0; i < cupList.length; i++) {
+        const cup = cupList[i];
+        const nextCup = cupList[(i + 1) % cupList.length];
+        map[cup] = nextCup;
     }
-    currentNode.next = map[cupList[0]];
 
     // play the game
-    let currentCupNode = map[cupList[0]];
     const maxCup = cupList.length;
+    let currentCup = cupList[0];
     for (let i = 0; i < iterations; i++) {
-        // console.log(`-- move ${i+1} --`);
-        // console.log(`cups (${currentCupNode.data}): ${}`);
 
-        const removedNodes = [currentCupNode.next, currentCupNode.next.next, currentCupNode.next.next.next];
-        currentCupNode.next = removedNodes[2].next;
+        // remove 3 cups
+        const a = map[currentCup];
+        const b = map[a];
+        const c = map[b];
+        map[currentCup] = map[c];
 
-        // console.log(`pickup: ${removedNodes[0].data}, ${removedNodes[1].data}, ${removedNodes[2].data}`);
-
-        // find destination node
+        // find destination cup
         let destinationCup;
-        let j = currentCupNode.data;
-        const a = removedNodes[0].data;
-        const b = removedNodes[1].data;
-        const c = removedNodes[2].data;
+        let j = currentCup;
         while (!destinationCup) {
             j--;
             if (j < 1) {
@@ -50,32 +39,25 @@ function compute2(cupList, iterations, part2) {
             }
         }
 
-        // console.log(`destination: ${destinationCup.data}`);
-
         // insert cups
-        const next = destinationCup.next;
-        destinationCup.next = removedNodes[0];
-        removedNodes[2].next = next;
+        map[c] = destinationCup;
+        map[j] = a;
 
-        // update currentCupNode
-        currentCupNode = currentCupNode.next;
-        // console.log(``);
+        // update currentCup
+        currentCup = map[currentCup];
     }
-
-    let node = map[1].next;
-    if (part2) {
-        return node.data * node.next.data;
-    }
-    let result = '';
-    while (node.data !== 1) {
-        result += node.data;
-        node = node.next;
-    }
-    return result;
+    return map;
 }
 
 function part1() {
-    return compute2([...cups], 100);
+    const map = compute([...cups], 100);
+    let result = '';
+    let i = map[1];
+    while (result.length < cups.length - 1) {
+        result += i;
+        i = map[i];
+    }
+    return result;
 }
 console.log(part1());
 
@@ -85,6 +67,7 @@ function part2() {
     while (input.length < 1000000) {
         input.push(i++);
     }
-    return compute2(input, 10000000, true);
+    const map = compute(input, 10000000);
+    return map[1] * map[map[1]];
 }
 console.log(part2());
